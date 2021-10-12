0f8ec14609eb1a9f79bff20a933f5451715fa158
SURBHI0402 <81684867+SURBHI0402@users.noreply.github.com>
2021-10-12 22:38:55 +0530

    uploaded
    done

---
+++ script.js
@@ -0,0 +1,13 @@
+function validate()
+{
+    var username=document.getElementById("username").nodevalue;
+    var password=DOCUMENT.getElementById("password").nodevalue;
+    if(username=="surbhi"&&password=="raut")
+    {alert("login succesfully");
+    return false;
+    }
+    else
+    {
+        alert("login failed");
+    }
+}
